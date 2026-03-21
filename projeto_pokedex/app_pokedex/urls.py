9c9a5300b03dce5ebeb031485f78d27ea74a9109
Caique Brito <gameroff12345@gmail.com>
2026-03-20 22:56:33 -0300

    chore: configura settings e urls para servir arquivos de midia
    - MEDIA_URL e MEDIA_ROOT configurados no settings.py
    
    - static adicionado no urlpatterns do projeto para servir midia em desenvolvimento

--- projeto_pokedex/app_pokedex/urls.py
+++ projeto_pokedex/app_pokedex/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.urls import include, path
 from . import views
 from rest_framework import routers
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'pokemon', views.PokemonViewSet)
@@ -8,4 +10,4 @@ router.register(r'pokemon', views.PokemonViewSet)
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
