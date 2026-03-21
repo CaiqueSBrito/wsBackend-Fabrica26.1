0588fc33d0dad9b6e78a93a7790be3503401ab98
Caique Brito <gameroff12345@gmail.com>
2026-03-21 17:37:49 -0300

    security: move sensitive settings to environment variables
    - move SECRET_KEY to .env
    
    - move DEBUG to .env
    
    - ensure .env is not tracked by git

--- projeto_pokedex/projeto_pokedex/settings.py
+++ projeto_pokedex/projeto_pokedex/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-16o^n@!32g8&fk2mk8m@iii#(=y3eada$b+lk3ak)&@7s)z0-r'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
